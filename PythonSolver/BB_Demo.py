57e6c9b62089151dc486fada9f7f5b7077463dbe
imgemp <imgemp@haar.cs.umass.edu>
2014-10-26 16:55:21 -0400

    Added solvers

--- PythonSolver/BB_Demo.py
+++ PythonSolver/BB_Demo.py
@@ -4,7 +4,12 @@ import numpy as np
 
 from Domains.BloodBank import *
 
+from Solvers.Euler import *
+from Solvers.Extragradient import *
+from Solvers.AcceleratedGradient import *
 from Solvers.HeunEuler import *
+from Solvers.AdamsBashforthEuler import *
+from Solvers.CashKarp import *
 
 from Solver import Solve
 from Options import *
@@ -20,11 +25,16 @@ def Demo():
     #__BLOOD_BANK__##################################################
 
     # Define Network and Domain
-    Network = CreateRandomNetwork(nC=2,nB=2,nD=2,nR=2,seed=0)
+    Network = CreateRandomNetwork(nC=2,nB=2,nD=2,nR=3,seed=0)
     Domain = BloodBank(Network=Network,alpha=2)
 
     # Set Method
-    Method = HeunEuler(Domain=Domain,P=RPlusProjection(),Delta0=1e-5)
+    # Method = Euler(Domain=Domain,P=RPlusProjection())
+    # Method = EG(Domain=Domain,P=RPlusProjection())
+    Method = AG(Domain=Domain,P=RPlusProjection())
+    # Method = HeunEuler(Domain=Domain,P=RPlusProjection(),Delta0=1e-5)
+    # Method = ABEuler(Domain=Domain,P=RPlusProjection(),Delta0=1e-5)
+    # Method = CashKarp(Domain=Domain,P=RPlusProjection(),Delta0=1e-6)
 
     # Initialize Starting Point
     Start = np.zeros(Domain.Dim)
@@ -33,9 +43,10 @@ def Demo():
     gap_0 = Domain.gap_rplus(Start)
 
 	# Set Options
-    Init = Initialization(Step=-1e-10)
-    Term = Termination(MaxIter=10000,Tols=[(Domain.gap_rplus,1e-6*gap_0)])
-    Repo = Reporting(Requests=[Domain.gap_rplus])
+    # Init = Initialization(Step=-1e-10)
+    Init = Initialization(Step=-0.1)
+    Term = Termination(MaxIter=25000,Tols=[(Domain.gap_rplus,1e-6*gap_0)])
+    Repo = Reporting(Requests=[Domain.gap_rplus,'Step','F Evaluations','Projections'])
     Misc = Miscellaneous()
     Options = DescentOptions(Init,Term,Repo,Misc)
 
