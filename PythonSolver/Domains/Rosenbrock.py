852cda4b01a458d0a2f039ce0e0a379aa98accbe
imgemp <imgemp@haar.cs.umass.edu>
2014-10-27 13:48:48 -0400

    Added the Rosenbrock Function + Demo
     - RK methods appear to be somewhat dimension free

---
+++ PythonSolver/Domains/Rosenbrock.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from Domain import Domain
+
+class Rosenbrock(Domain):
+
+    def __init__(self,Dim=2):
+        self.Dim = Dim+Dim%2
+        self.Min = 0.0
+
+    def f(self,Data):
+        return np.sum(100.*(Data[0::2]**2-Data[1::2])**2.+(Data[0::2]-1)**2.)
+
+    def F(self,Data):
+        F = np.zeros(Data.shape)
+        F[0::2] = 400.*Data[0::2]*(Data[0::2]**2.-Data[1::2])+2.*(Data[0::2]-1)
+        F[1::2] = -200.*(Data[0::2]**2.-Data[1::2])
+        return F
+
+    def f_Error(self,Data):
+        return self.f(Data)-self.Min
---
+++ PythonSolver/Rosenbrock_Demo.py
@@ -0,0 +1,71 @@
+import time
+import datetime
+import numpy as np
+
+from Domains.Rosenbrock import *
+
+from Solvers.Euler import *
+from Solvers.Extragradient import *
+from Solvers.AcceleratedGradient import *
+from Solvers.HeunEuler import *
+from Solvers.AdamsBashforthEuler import *
+from Solvers.CashKarp import *
+
+from Solver import Solve
+from Options import *
+from Log import *
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+def Demo():
+
+    #__ROSENBROCK__##################################################
+
+    # Define Domain
+    Domain = Rosenbrock(Dim=1000)
+
+    # Set Method
+    # Method = Euler(Domain=Domain,P=RPlusProjection())
+    # Method = EG(Domain=Domain,P=RPlusProjection())
+    # Method = AG(Domain=Domain,P=RPlusProjection())
+    # Method = HeunEuler(Domain=Domain,P=RPlusProjection(),Delta0=1e-6)
+    # Method = ABEuler(Domain=Domain,P=RPlusProjection(),Delta0=1e-5)
+    Method = CashKarp(Domain=Domain,P=RPlusProjection(),Delta0=1e-6)
+
+    # Initialize Starting Point
+    Start = -0.5*np.ones(Domain.Dim)
+
+	# Set Options
+    Init = Initialization(Step=-1e-10)
+    # Init = Initialization(Step=-0.1)
+    Term = Termination(MaxIter=20000,Tols=[(Domain.f_Error,1e-6)])
+    Repo = Reporting(Requests=[Domain.f_Error,'Step','F Evaluations','Projections'])
+    Misc = Miscellaneous()
+    Options = DescentOptions(Init,Term,Repo,Misc)
+
+    # Print Stats
+    PrintSimStats(Domain,Method,Options)
+
+    # Start Solver
+    tic = time.time()
+    Rosenbrock_Results = Solve(Start,Method,Domain,Options)
+    toc = time.time() - tic
+
+    # Print Results
+    PrintSimResults(Options,Rosenbrock_Results,Method,toc)
+
+    # Zero Projections for Later Use
+    Method.Proj.NP = 0
+
+if __name__ == '__main__':
+  Demo()
+
+
+
+
+
+
+
