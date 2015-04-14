eb9057c890794068cadea2db6a0cb001e5da594d
Clemens <cgbr@cs.umass.edu>
2015-04-14 00:25:34 -0400

    NE Guesser, Update 1

--- Solvers/BoostedWPL.py
+++ Solvers/BoostedWPL.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 __author__ = 'clemens'
 
-import numpy as np
+# import numpy as np
 from Projection import *
 from Solvers.Solver import Solver
 from Utilities import *
 import config
 from Options import Reporting
-from Estimators import decaying_average_estimator
+# from Estimators import decaying_average_estimator
 
 
 class BoostedWPL(Solver):
@@ -42,6 +42,9 @@ class BoostedWPL(Solver):
         # self.ne_hypotheses = np.zeros(self.value_approx_range.shape)
 
     def init_temp_storage(self, start, domain, options):
+        self.temp_storage['Learning Settings'] = [self.learning_settings] * 2
+        self.temp_storage['Learning Settings'] = [self.temp_storage['Learning Settings'],
+                                                  self.temp_storage['Learning Settings']]
         self.temp_storage['Action'] = np.zeros((self.storage_size,
                                                 # self.domain.players,
                                                 2,
@@ -50,7 +53,7 @@ class BoostedWPL(Solver):
         self.temp_storage['Policy Learning Rate'] = (np.ones((self.storage_size,
                                                               # self.domain.players,
                                                               2,
-                                                              self.get_forecaster_no('m')))
+                                                              self.get_forecaster_no(option='m')))
                                                      * self.lr).tolist()
         self.temp_storage['Policy Gradient (dPi)'] = self.storage_size * [np.zeros(domain.b.shape)]
         self.temp_storage['Reward'] = np.zeros((self.storage_size,
@@ -60,7 +63,7 @@ class BoostedWPL(Solver):
         self.temp_storage['Value Function'] = np.zeros((self.storage_size,
                                                         # self.domain.players,
                                                         2,
-                                                        self.get_forecaster_no('m'),
+                                                        self.get_forecaster_no(option='m'),
                                                         )).tolist()
         self.temp_storage['True Value Function'] = np.zeros((self.storage_size,
                                                              # self.domain.players,
@@ -69,7 +72,7 @@ class BoostedWPL(Solver):
         self.temp_storage['Forecaster Policies'] = (np.zeros((self.storage_size,
                                                               # self.domain.players,
                                                               2,
-                                                              self.get_forecaster_no('m'),
+                                                              self.get_forecaster_no(option='m'),
                                                               # self.domain.Dim,
                                                               2,
                                                               ))/2.)
@@ -79,7 +82,7 @@ class BoostedWPL(Solver):
                                                     51,
                                                     )).tolist()
         for i in range(2):
-            for j in range(self.get_forecaster_no('w')):
+            for j in range(self.get_forecaster_no(option='w')):
                 self.temp_storage['Forecaster Policies'][:, i, j, :] = np.array(start[i])
         self.temp_storage['Forecaster Policies'] = self.temp_storage['Forecaster Policies'].tolist()
         return self.temp_storage
@@ -119,7 +122,7 @@ class BoostedWPL(Solver):
         # computing the policy gradient and the learning rate
         return policy_gradient, self.Proj.p(forecaster_policy, learning_setting[2], policy_gradient)
 
-    def weighted_average_forecaster(self, fc_policies, reward_history, action_history, averaging_type=None):
+    def weighted_average_forecaster(self, fc_policies, reward_history, action_history, player, averaging_type=None):
         # calculating the reward-action space
         fcp = np.array([fc_policies[i, :, action_history[i]]*(.5**(len(action_history)-i-1)) for i in range(len(action_history))])
         weighted_reward = np.array(reward_history)
@@ -133,62 +136,87 @@ class BoostedWPL(Solver):
             reward_action_function *= weight
             reward_action_function = np.exp(reward_action_function)
         best_estimate = np.dot(fc_policies[-1].T, reward_action_function[None].T)
-        best_estimate = best_estimate.T[-1]/(np.sum(best_estimate)+1e-9)
         # returning after projecting back on the simplex
-        if self.get_forecaster_no('r') > self.get_forecaster_no('w'):
-            best_ne_index = self.get_forecaster_no('w') + reward_action_function[self.get_forecaster_no('r')-1:].argmax()
+        best_estimate = best_estimate.T[-1]/(np.sum(best_estimate)+1e-9)
+
+        # testing the quality of our ne's
+        # do we have any ne hypotheses?
+        if self.get_forecaster_no(player, 'r') > self.get_forecaster_no(option='w'):
+            print('yeyeyey')
+            # which is the best one?
+            best_ne_index = self.get_forecaster_no(option='w') + \
+                            reward_action_function[self.get_forecaster_no(option='w'):].argmax()
+            # is the best one in accordance with the current game behavior?
             if np.linalg.norm(best_estimate-fc_policies[-1, best_ne_index]) < .7:
                 return fc_policies[-1, best_ne_index]
         return best_estimate
 
-    def equilibrium_testing(self, fc_policies, ne_hypos, decision_type='majority', iteration=0):
+    def equilibrium_testing(self, fc_policies, ne_hypos, player, decision_type='majority', iteration=0):
         """
         given a set of policies, this method decides on whether or not an equilibrium has been found, based
         on the deviation of the different policy hypotheses.
         :param fc_policies:
         :return:
         """
+        # initializing variables
+        learning_settings = list(self.temp_storage['Learning Settings'][-1][player])
         # 1. computing the symmetrical distance matrix between the different hypotheses
-        distances = np.zeros((self.get_forecaster_no('w'), self.get_forecaster_no('w')))
-        for i in range(self.get_forecaster_no('w')):
-            for j in range(i+1, self.get_forecaster_no('w'), 1):
+        distances = np.zeros((self.get_forecaster_no(player, 'w'), self.get_forecaster_no(player, 'w')))
+        for i in range(self.get_forecaster_no(player, 'w')):
+            for j in range(i+1, self.get_forecaster_no(player, 'w'), 1):
                 distances[i, j] = np.linalg.norm(fc_policies[i] - fc_policies[j])
-        if decision_type == 'majority' and iteration > 50:
-            if np.sum(distances > .02) < 5:
-                # exception = np.mean(distances > .1, axis=0).argmax()
-                # if config.debug_output_level > 2:
-                hypo_index = compute_value_index(self.value_approx_range,
-                                                 np.mean(fc_policies[:self.get_forecaster_no('w')], axis=0)[0])
-                ne_hypos = add_value_vector(ne_hypos, self.additive_, hypo_index)
-                # print np.round(np.mean(fc_policies[:self.get_forecaster_no('w')], axis=0), 2), iteration
-                # print ne_hypos
-                if ne_hypos.max() > .5:
-                    # print self.value_approx_range[ne_hypos.argmax()], ne_hypos.max()
-                    pol = np.array([self.value_approx_range[ne_hypos.argmax()],
-                                    1-self.value_approx_range[ne_hypos.argmax()]])
-                    # self.get_forecaster_no('w') += 1
-                    # TODO: making sure that we do not add the same ne forecaster again
-                    # if we have seen it, update it instead
-                    # print('the policies:\n', fc_policies[self.get_forecaster_no('w'):])
-                    # print('the policies:\n', fc_policies-pol)
-                    # print('distances:', np.sqrt(np.sum(np.square(fc_policies[self.get_forecaster_no('w'):]-pol), axis=1)))
-                    if np.sqrt(np.sum(np.square(fc_policies[self.get_forecaster_no('w'):]-pol), axis=1)).min() > .6:
-                        # if we have not seen it, add it
-                        self.learning_settings.append([1., 1., .0])
-                        # print(fc_policies, self.get_forecaster_no('r'), fc_policies[self.get_forecaster_no('r')])
-                        fc_policies[self.get_forecaster_no('r')-1] = pol
-                        # print('added a new ne hypothesis:', pol)
-                    else:
-                        nep = np.sqrt(np.sum(np.square(fc_policies[self.get_forecaster_no('w'):]-pol), axis=0)).argmin()
-                        fc_policies[self.get_forecaster_no('w')+nep] = (pol + fc_policies[self.get_forecaster_no('w')+nep])/2
-                        # print('modified the hypothesis', fc_policies[self.get_forecaster_no('w')+nep], 'to')
-                    # print(self.value_approx_range[self.ne_hypotheses.argmax()])
-                    ne_hypos *= .99
-        return ne_hypos, fc_policies
 
-    def get_forecaster_no(self, option='r'):
-        if option == 'r':
-            return len(self.learning_settings)
+        # 1b. checking that we do not have the same hypothesis twice:
+        no_forecasters = self.get_forecaster_no(player, 'r') - self.get_forecaster_no(player, 'w')
+        hypo_distances = np.ones((no_forecasters, no_forecasters))
+        # computing the matrix of distances between the forecasters
+        for i in range(no_forecasters):
+            for j in range(i+1, no_forecasters, 1):
+                hypo_distances[i, j] = np.linalg.norm(fc_policies[self.get_forecaster_no(player, 'w')+i] -
+                                                      fc_policies[self.get_forecaster_no(player, 'w')+j])
+        if hypo_distances != [] and hypo_distances.min() < .1:
+            hypo_ind = np.unravel_index(hypo_distances.argmin(), hypo_distances.shape)
+            index_to_remove = self.get_forecaster_no(player, option='w') + max(hypo_ind)
+            print('lsb', learning_settings,
+                  fc_policies[index_to_remove],
+                  fc_policies[self.get_forecaster_no(player, option='w') + min(hypo_ind)])
+            learning_settings.pop(index_to_remove)
+            fc_policies[index_to_remove] = np.zeros(np.array(fc_policies[index_to_remove]).shape).tolist()
+            print('lsa', learning_settings, fc_policies[self.get_forecaster_no(player, option='w') + min(hypo_ind)])
+
+        # 2. deciding on whether or not the current policies agree according to the chosen strategy
+        if decision_type == 'majority' and iteration > self.exploration_trials:
+            # the following computes the number of distances greater than a specific threshold
+            if np.sum(distances > .02) < 5:
+                # this computes the value of a nash equilibrium hypothesis
+                hypo_index = compute_value_index(self.value_approx_range,
+                                                 np.mean(fc_policies[:self.get_forecaster_no(player, 'w')], axis=0)[0])
+                # this computes the value function for the hypothesis
+                ne_hypos = add_value_vector(ne_hypos, self.additive_, hypo_index)
+                # this computes whether or not the hypothesis is close enough
+                if ne_hypos.max() > .5:
+                    # if it is, this converts the hypothesis to a policy
+                    pol = np.mean(fc_policies[:self.get_forecaster_no(option='w')+1], axis=0)
+                    # print(pol)
+                    # TODO: making sure that we do not add the same ne forecaster again
+                    # is this a new hypothesis?
+                    if np.sqrt(np.sum(np.square(np.array(fc_policies[self.get_forecaster_no(player, 'w'):])-pol),
+                                      axis=1)).min() > .6:
+                        # yes - the distance to the old hypotheses is too large
+                        learning_settings.append([1., 1., .0])
+                        fc_policies[self.get_forecaster_no(player, 'r')-1] = pol
+                    else:
+                        # no - the policy is nearly the same as a previous hypothesis
+                        nep = np.sqrt(np.sum(np.square(fc_policies[self.get_forecaster_no(player, 'w'):]-pol), axis=0)).argmin()
+                        fc_policies[self.get_forecaster_no(player, 'w')+nep] = (ne_hypos.max()*pol + self.additive_.max()*fc_policies[self.get_forecaster_no(player, 'w')+nep])/(ne_hypos.max() + self.additive_.max())
+                    # decrease the value of everything in the value funtion
+                    ne_hypos *= .99
+
+        return ne_hypos, fc_policies, learning_settings
+
+    def get_forecaster_no(self, player=None, option='r'):
+        if option == 'r' and player is not None:
+            return len(self.temp_storage['Learning Settings'][-1][player])
         if option == 'w':
             return self.no_forecasters
         if option == 'm':
@@ -204,6 +232,7 @@ class BoostedWPL(Solver):
         tmp_value = self.temp_storage['Value Function'][-1]
         ne_estimates = np.array(self.temp_storage['NE guesser'][-1])
         tmp_forecaster_policies = np.array(self.temp_storage['Forecaster Policies'][-1*iteration:])
+        tmp_learning_settings = self.temp_storage['Learning Settings'][-1]
 
         # -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~-
         # Initialize Storage
@@ -219,30 +248,28 @@ class BoostedWPL(Solver):
         # -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~-
         # 1. start by calculating the forecaster's policy/recommendation
         for player in range(self.domain.players):
-            ne_estimates[player], updated_forecaster_policies[player] = self.equilibrium_testing(updated_forecaster_policies[player],
-                                                                                                 ne_estimates[player],
-                                                                                                 'majority',
-                                                                                                 iteration)
+            ne_estimates[player], \
+            updated_forecaster_policies[player], \
+            tmp_learning_settings[player] = self.equilibrium_testing(updated_forecaster_policies[player],
+                                                                     ne_estimates[player],
+                                                                     player,
+                                                                     'majority',
+                                                                     iteration)
             policy_taken[player] = self.weighted_average_forecaster(tmp_forecaster_policies[:, player],
                                                                     tmp_reward[:, player],
                                                                     tmp_action[:, player],
+                                                                    player,
                                                                     averaging_type='exponential')
-            # policy_value_distribution = self.weighted_average_forecaster(tmp_forecaster_policies[:, player],
-            #                                                              tmp_reward[:, player],
-            #                                                              tmp_action[:, player])
-            # print policy_value_distribution
-            # policy_taken[player] = updated_forecaster_policies[player][policy_value_distribution.argmax()]
-            # policy_taken[player] = updated_forecaster_policies[player][0]
 
         # stupid random play
-        if iteration <= 500:
-            policy_taken[0] = [1., 0.]
-        elif iteration <= 1000:
-            policy_taken[0] = [0., 1.]
-        elif iteration % 80 < 40:
-            policy_taken[0] = [0., 1.]
-        else:
-            policy_taken[0] = [1., 0.]
+        # if iteration <= 1500:
+        #     policy_taken[0] = [1., 0.]
+        # elif iteration <= 3000:
+        #     policy_taken[0] = [0., 1.]
+        # elif iteration % 160 < 80:
+        #     policy_taken[0] = [0., 1.]
+        # else:
+        #     policy_taken[0] = [1., 0.]
 
         # -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~- -~*#*~-
         # 2. then play the game
@@ -265,7 +292,7 @@ class BoostedWPL(Solver):
         # perform the update on the policies:
         for player in range(2):
             # iterating over the different forecasters
-            for i in range(self.get_forecaster_no('w')):
+            for i in range(self.get_forecaster_no(option='w')):
                 # compute the value of the current strategy
                 value[player][i] = self.compute_value_function(reward[player], tmp_value[player][i])
                 policy_gradient[player], \
@@ -298,6 +325,7 @@ class BoostedWPL(Solver):
         temp_data['Action'] = action
         temp_data['Forecaster Policies'] = updated_forecaster_policies
         temp_data['NE guesser'] = ne_estimates
+        temp_data['Learning Settings'] = tmp_learning_settings
         self.book_keeping(temp_data)
 
         return self.temp_storage
