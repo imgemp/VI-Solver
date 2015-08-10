99f3859aff10c8bc9e8fd5f8ac9247b5d9ed8dba
imgemp <imgemp@haar.cs.umass.edu>
2015-08-10 17:27:42 -0400

    Clean up

--- Demo_Lienard_BofA.py
+++ Demo_Lienard_BofA.py
@@ -13,9 +13,6 @@ from VISolver.Options import (
     DescentOptions, Miscellaneous, Reporting, Termination, Initialization)
 from VISolver.Log import PrintSimStats
 
-# from VISolver.Utilities import ListONP2NP, aug_grid, int2ind, ind2int, ind2pt,
-#     neighbors, pairwise, update_LamRef, adjustLams2Ref, update_Prob_Data, \
-#     MCLE_BofA_Identification
 from VISolver.Utilities import ListONP2NP, aug_grid, MCLE_BofA_ID_par
 
 from matplotlib import pyplot as plt
@@ -47,7 +44,6 @@ def Demo():
     # Print Stats
     PrintSimStats(Domain,Method,Options)
 
-    # grid = [np.array([.1,10.,6])]*5+[np.array([.1,1.,6])]*5
     grid = [np.array([-2.5,2.5,51])]*2
     grid = ListONP2NP(grid)
     grid = aug_grid(grid)
@@ -57,16 +53,6 @@ def Demo():
                                eta_1=1.2,eta_2=.95,eps=1.,
                                L=50,q=2,r=1.1,Dinv=Dinv)
     ref, data, p, i, avg = results
-
-    # for key, datum in data.iteritems():
-    #     print(key)
-    #     for sample in datum:
-    #         print(sample)
-
-    for LE in ref:
-        print(LE)
-        for sample in data[hash(str(LE))]:
-            print(sample)
 
     for sample in data[hash(str(ref[0]))]:
         plt.plot([sample[0][0]],[sample[0][1]],'*r')
