fb2fe081d386f501c22d2606d41eb611898ae46f
Ian Gemp <imgemp@gmail.com>
2015-08-14 07:22:24 -0400

    Reduce to 8 nodes

--- Demo_Cloud_BofA.py
+++ Demo_Cloud_BofA.py
@@ -55,9 +55,9 @@ def Demo():
     grid = aug_grid(grid)
     Dinv = np.diag(1./grid[:,3])
 
-    results = MCLE_BofA_ID_par2(sim,args,grid,nodes=25,limit=20,AVG=.01,
+    results = MCLE_BofA_ID_par2(sim,args,grid,nodes=8,limit=20,AVG=.01,
                                 eta_1=1.2,eta_2=.95,eps=1.,
-                                L=25,q=2,r=1.1,Dinv=Dinv)
+                                L=8,q=2,r=1.1,Dinv=Dinv)
     ref, data, p, i, avg, bndry_ids = results
 
     # plt.figure()
