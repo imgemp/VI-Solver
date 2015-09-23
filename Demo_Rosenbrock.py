0a3cca2fa91d30aa03eca9eff2935b95e61cf666
Ian Gemp <imgemp@users.noreply.github.com>
2015-09-23 09:37:52 -0400

    Update Demo_Rosenbrock.py

--- Demo_Rosenbrock.py
+++ Demo_Rosenbrock.py
@@ -1,6 +1,10 @@
 import time
 import numpy as np
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
 from VISolver.Domains.Rosenbrock import Rosenbrock
 
 from VISolver.Solvers.CashKarp import CashKarp
@@ -28,7 +32,7 @@ def Demo():
     Init = Initialization(Step=-1e-10)
     Term = Termination(MaxIter=20000,Tols=[(Domain.f_Error,1e-6)])
     Repo = Reporting(Requests=[Domain.f_Error, 'Step', 'F Evaluations',
-                               'Projections'])
+                               'Projections','Data'])
     Misc = Miscellaneous()
     Options = DescentOptions(Init,Term,Repo,Misc)
 
@@ -43,8 +47,29 @@ def Demo():
     # Print Results
     PrintSimResults(Options,Rosenbrock_Results,Method,toc)
 
-    # Zero Projections for Later Use
-    Method.Proj.NP = 0
+    # Plot Results
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    X = np.arange(-2, 2, 0.25)
+    Y = np.arange(-1, 3, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.zeros_like(X)
+    for i in xrange(Z.shape[0]):
+        for j in xrange(Z.shape[1]):
+            Z[i,j] = Domain.f(np.array([X[i,j],Y[i,j]]))
+    ax.plot_surface(X,Y,Z,rstride=1,cstride=1,cmap=cm.coolwarm,
+                    linewidth=0,antialiased=False)
+    data = Rosenbrock_Results.PermStorage['Data']
+    res = Rosenbrock_Results.PermStorage[Domain.f_Error]
+    trajX = []
+    trajY = []
+    trajZ = []
+    for i in xrange(len(data)):
+        trajX.append(data[i][0])
+        trajY.append(data[i][1])
+        trajZ.append(res[i])
+    ax.plot(trajX,trajY,trajZ)
+    plt.show()
 
 if __name__ == '__main__':
     Demo()
